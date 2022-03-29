1e946be0235f53ca51443afc620ea1069444aeea
captproton <carl@wdwhub.net>
2022-03-28 17:34:58 -0700

    ADD: contact_message form

--- app/controllers/contact_messages_controller.rb
+++ app/controllers/contact_messages_controller.rb
@@ -1,23 +1,12 @@
 class ContactMessagesController < ApplicationController
-  before_action :set_contact_message, only: %i[ show edit update destroy ]
+  layout "frontdoor"
+  # before_action :set_contact_message, only: %i[ show ]
 
   # GET /contact_messages or /contact_messages.json
   def index
-    @contact_messages = ContactMessage.all
+    # @contact_messages = ContactMessage.all
   end
 
-  # GET /contact_messages/1 or /contact_messages/1.json
-  def show
-  end
-
-  # GET /contact_messages/new
-  def new
-    @contact_message = ContactMessage.new
-  end
-
-  # GET /contact_messages/1/edit
-  def edit
-  end
 
   # POST /contact_messages or /contact_messages.json
   def create
@@ -25,7 +14,8 @@ class ContactMessagesController < ApplicationController
 
     respond_to do |format|
       if @contact_message.save
-        format.html { redirect_to contact_message_url(@contact_message), notice: "Contact message was successfully created." }
+        _notify_receipt_of_message(@contact_message)
+        format.html { redirect_to contact_messages_url, notice: "Your message was successfully sent." }
         format.json { render :show, status: :created, location: @contact_message }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -34,34 +24,16 @@ class ContactMessagesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /contact_messages/1 or /contact_messages/1.json
-  def update
-    respond_to do |format|
-      if @contact_message.update(contact_message_params)
-        format.html { redirect_to contact_message_url(@contact_message), notice: "Contact message was successfully updated." }
-        format.json { render :show, status: :ok, location: @contact_message }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @contact_message.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /contact_messages/1 or /contact_messages/1.json
-  def destroy
-    @contact_message.destroy
-
-    respond_to do |format|
-      format.html { redirect_to contact_messages_url, notice: "Contact message was successfully destroyed." }
-      format.json { head :no_content }
-    end
+  def  _notify_receipt_of_message(contact_message)
+    # contact_message details are delivered via ContactFormNotification
+    # ex: ContactFormNotification.with({contact_message: contact_message}).deliver(User.last)
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
-    def set_contact_message
-      @contact_message = ContactMessage.find(params[:id])
-    end
+    # def set_contact_message
+    #   @contact_message = ContactMessage.find(params[:id])
+    # end
 
     # Only allow a list of trusted parameters through.
     def contact_message_params
